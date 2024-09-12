19288b90fdcf69fc640c198083bada3ae22b88ff
r-spiewak <63987228+r-spiewak@users.noreply.github.com>
2024-09-12 13:18:14 -0400

    Fix for SARMIAX class

--- src/sync_project/forecasting/statistics/sarimax.py
+++ src/sync_project/forecasting/statistics/sarimax.py
@@ -5,6 +5,13 @@ from statsmodels.tsa.statespace.sarimax import SARIMAX as sarimax
 
 from .statsmodels_wrapper import StatsModelsWrapper
 
+# There's an issue with using the predict function here,
+# since this class supplies the y data as the exog
+# variable which then must also be supplied as exog in
+# the predict method. That is not done, at present.
+# It works if I remove the instantiate method from here
+# and just rely on the one in the base class though.
+
 
 class SARIMAX(StatsModelsWrapper):
     """Wrapper class for SARIMAX model from statsmodels."""
@@ -13,8 +20,8 @@ class SARIMAX(StatsModelsWrapper):
         """Return the SARIMAX class."""
         return sarimax
 
-    def _instantiate_model(self, X, y=None):
-        """Instantiate the SARIMAX model with the parameters passed in the constructor.
-        SARIMAX can take exogenous variables as well, so `y` could be exog data.
-        """
-        return sarimax(X, exog=y, **self.model_init_params)
+    # def _instantiate_model(self, X, y=None):
+    #     """Instantiate the SARIMAX model with the parameters passed in the constructor.
+    #     SARIMAX can take exogenous variables as well, so `y` could be exog data.
+    #     """
+    #     return sarimax(X, exog=y, **self.model_init_params)
